eb15e1f5aafccca8ae28d862a47bd1e97531a3bd
gamilsaad92 <gamilsaad92@gmail.com>
2025-06-13 21:35:15 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,21 @@
 // src/App.jsx
 
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import DashboardLayout from './components/DashboardLayout';
+mport LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import { AuthContext } from './main';
 
 export default function App() {
+  const { session } = useContext(AuthContext);
+  const [signUp, setSignUp] = useState(false);
+
+  if (!session) {
+    return signUp ? (
+      <SignUpForm onSwitch={() => setSignUp(false)} />
+    ) : (
+      <LoginForm onSwitch={() => setSignUp(true)} />
+    );
+  }
   return <DashboardLayout />;
 }
