51928bf6fbbb6c1ffa1c6763b61e58028ee00a35
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-09 22:40:11 -0700

    Update Form1140Stepper.tsx

--- ui/src/components/Form1140/Form1140Stepper.tsx
+++ ui/src/components/Form1140/Form1140Stepper.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import PartINotification from './PartINotification'
+import PartIFollowUp from './PartIFollowUp'
+import PartIIRestoration from './PartIIRestoration'
+import AttachmentsAndReview from './AttachmentsAndReview'
 
 export default function Form1140Stepper() {
   const [step, setStep] = useState(1)
@@ -10,10 +13,17 @@ export default function Form1140Stepper() {
     setStep(step + 1)
   }
 
+  const submit = () => {
+    console.log("Final Data Submission:", formData)
+    // TODO: Submit to backend
+  }
+
   return (
     <div className="max-w-3xl mx-auto">
       {step === 1 && <PartINotification onNext={next} defaultValues={formData} />}
-      {step > 1 && <div>Next steps will be rendered here…</div>}
+      {step === 2 && <PartIFollowUp onNext={next} defaultValues={formData} />}
+      {step === 3 && <PartIIRestoration onNext={next} defaultValues={formData} />}
+      {step === 4 && <AttachmentsAndReview formData={formData} onSubmit={submit} />}
     </div>
   )
 }
