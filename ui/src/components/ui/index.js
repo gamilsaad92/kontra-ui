3cedd68b0d1d31b52db8dce4a7ac7540824fffe1
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-12 14:53:04 -0700

    Create index.js

---
+++ ui/src/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './Button.jsx';
+export { default as Card } from './Card.jsx';
+export { default as Table } from './Table.jsx';
+export { default as FormField } from './FormField.jsx';
+export { default as Input } from './Input.jsx';
