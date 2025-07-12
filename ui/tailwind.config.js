b130e3f843c6d674bd498a11b349afe3535c6ca2
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-12 14:55:08 -0700

    Update tailwind.config.js

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,7 +1,14 @@
+import theme from './src/theme.js';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+     extend: {
+      colors: theme.colors,
+      spacing: theme.spacing,
+      fontFamily: theme.fontFamily,
+      fontSize: theme.fontSize,
+    },
   },
   plugins: [],
 }
