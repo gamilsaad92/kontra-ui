291eae4274aa1a8e6be6a03b83ade235cbfc299a
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-21 16:29:33 -0500

    Update analytics-orders.test.js

--- api/analytics-orders.test.js
+++ api/analytics-orders.test.js
@@ -3,7 +3,9 @@ const app = require('./index');
 
 describe('GET /api/analytics/orders', () => {
   it('returns analytics', async () => {
-    const res = await request(app).get('/api/analytics/orders');
+       const res = await request(app)
+      .get('/api/analytics/orders')
+      .set('X-Org-Id', '1');
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty('totalOrders');
   });
