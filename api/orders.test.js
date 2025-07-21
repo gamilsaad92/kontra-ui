64e3dec1c0fbc02c5955822e167830050b252d68
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-21 16:32:26 -0500

    Update orders.test.js

--- api/orders.test.js
+++ api/orders.test.js
@@ -2,8 +2,16 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('POST /api/orders', () => {
-  it('requires items', async () => {
+   it('requires organization id', async () => {
     const res = await request(app).post('/api/orders').send({});
+    expect(res.statusCode).toBe(400);
+  });
+  
+  it('requires items', async () => {
+    const res = await request(app)
+      .post('/api/orders')
+      .set('X-Org-Id', '1')
+      .send({});
     expect(res.statusCode).toBe(400);
   });
 });
