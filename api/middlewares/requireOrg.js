a2d68fe48860c5b72823b3cca94c6d668affc872
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-21 16:30:07 -0500

    Create requireOrg.js

---
+++ api/middlewares/requireOrg.js
@@ -0,0 +1,8 @@
+module.exports = function requireOrg(req, res, next) {
+  const orgId = req.organizationId || req.headers['x-org-id'];
+  if (!orgId) {
+    return res.status(400).json({ message: 'Missing organization id' });
+  }
+  req.orgId = orgId;
+  next();
+};
