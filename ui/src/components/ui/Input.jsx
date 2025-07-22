40f815d64843b0a91c8bb7cba6bd411798b51f8b
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-21 23:29:28 -0700

    Update Input.jsx

--- ui/src/components/ui/Input.jsx
+++ ui/src/components/ui/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Input({ className = '', ...props }) {
   return (
     <input
-      className={`w-full border rounded p-xs focus:ring-2 focus:ring-primary ${className}`}
+         className={`w-full border rounded p-xs focus:ring-2 focus:ring-primary focus:ring-offset-2 ${className}`}
       {...props}
     />
   );
