0900deeb0d8d5bf0a57298dae7d1cc418d43bacb
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-22 12:50:40 -0500

    Create WorkflowBuilder.test.jsx

---
+++ ui/src/components/__tests__/WorkflowBuilder.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import WorkflowBuilder from '../WorkflowBuilder';
+
+test('renders Save Workflow button', () => {
+  render(<WorkflowBuilder />);
+  expect(screen.getByText(/Save Workflow/)).toBeInTheDocument();
+});
