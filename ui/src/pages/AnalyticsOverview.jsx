7ebb6443e70e1eef2d53a7a7d16d765b3f1a431a
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-28 12:13:29 -0500

    Update AnalyticsOverview.jsx

--- ui/src/pages/AnalyticsOverview.jsx
+++ ui/src/pages/AnalyticsOverview.jsx
@@ -1,3 +1,11 @@
+import AnomalyDetectionCard from '../components/AnomalyDetectionCard';
+import PayoffAccelerationCard from '../components/PayoffAccelerationCard';
+
 export default function AnalyticsOverview() {
-  return <div>Analytics Overview content coming soon.</div>;
+   return (
+    <div className="space-y-4">
+      <AnomalyDetectionCard />
+      <PayoffAccelerationCard />
+    </div>
+  );
 }
