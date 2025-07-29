d2e0747526204d92bd5f3b1c35b646ad528e3bf8
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-28 22:33:54 -0700

    Update Servicing.jsx

--- ui/src/pages/Servicing.jsx
+++ ui/src/pages/Servicing.jsx
@@ -1,3 +1,15 @@
+import PaymentPortal from '../components/PaymentPortal';
+import SelfServicePayment from '../components/SelfServicePayment';
+import EscrowDashboard from '../components/EscrowDashboard';
+import CollectionsTable from '../components/CollectionsTable';
+
 export default function Servicing() {
-  return <div>Servicing content coming soon.</div>;
+ return (
+    <div className="space-y-4">
+      <PaymentPortal />
+      <SelfServicePayment />
+      <EscrowDashboard />
+      <CollectionsTable />
+    </div>
+  );
 }
