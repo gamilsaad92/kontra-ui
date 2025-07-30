0541a92b35f5a1a3da26ae5a552943d6ddaf7a2e
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-29 22:20:21 -0700

    Update GuestOccupancyCard.jsx

--- ui/src/modules/dashboard/GuestOccupancyCard.jsx
+++ ui/src/modules/dashboard/GuestOccupancyCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import GuestOccupancyChart from '../../components/GuestOccupancyChart';
 import Card from '../../components/Card';
 
-export default function GuestOccupancyCard({ to }) {
+export default function GuestOccupancyCard({ data, to }) {
   return (
-     <Card title="Guest Occupancy" to={to}>
-      <GuestOccupancyChart />
-      </Card>
+    <Card title="Guest Occupancy" to={to}>
+      <GuestOccupancyChart data={data} />
+    </Card>
   );
 }
