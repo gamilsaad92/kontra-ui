1eaa2d1860fb73022c42a3b8e3e4223d0aee1e56
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-31 16:03:37 -0500

    Create LiveAnalytics.jsx

---
+++ ui/src/pages/LiveAnalytics.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RealTimeAnalyticsDashboard from '../components/RealTimeAnalyticsDashboard.jsx';
+
+export default function LiveAnalytics() {
+  return <RealTimeAnalyticsDashboard />;
+}
