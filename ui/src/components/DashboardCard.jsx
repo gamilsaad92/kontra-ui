cfc5150d85bea12f231d991a93d41c9affc2090d
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-30 22:48:25 -0700

    Update DashboardCard.jsx

--- ui/src/components/DashboardCard.jsx
+++ ui/src/components/DashboardCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function DashboardCard({ icon, title, subtitle }) {
   return (
-    <div className="bg-gray-800 rounded shadow p-4 flex space-x-4 items-center">
-      <div className="w-12 h-12 rounded-full bg-blue-500 flex items-center justify-center text-xl">
+     <div className="bg-gray-800 rounded shadow p-4 flex items-center space-x-4">
+      <div className="w-12 h-12 rounded-full bg-brand flex items-center justify-center text-xl text-white">
         {icon}
       </div>
       <div>
