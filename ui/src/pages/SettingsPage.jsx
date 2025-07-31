b2fc8929102d001b94562fb276cca2539fec506b
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-30 23:00:59 -0700

    Create SettingsPage.jsx

---
+++ ui/src/pages/SettingsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import OrganizationSettings from '../components/OrganizationSettings.jsx';
+
+export default function SettingsPage() {
+  return (
+    <div className="p-6 text-white">
+      <OrganizationSettings />
+    </div>
+  );
+}
