f7fcf14a4e8a23d7bcc9c2b36fa49540ef465300
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-30 23:00:28 -0700

    Create MarketAnalysisPage.jsx

---
+++ ui/src/pages/MarketAnalysisPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketAnalysis from '../components/MarketAnalysis.jsx';
+
+export default function MarketAnalysisPage() {
+  return (
+    <div className="p-6 text-white">
+      <MarketAnalysis />
+    </div>
+  );
+}
