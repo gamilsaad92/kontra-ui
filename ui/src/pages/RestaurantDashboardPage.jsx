04088de0d9f4c248480e3e050194a1c9a097ccda
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-31 16:04:59 -0500

    Create RestaurantDashboardPage.jsx

---
+++ ui/src/pages/RestaurantDashboardPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StaffRestaurantDashboard from '../components/StaffRestaurantDashboard.jsx';
+
+export default function RestaurantDashboardPage() {
+  return <StaffRestaurantDashboard />;
+}
