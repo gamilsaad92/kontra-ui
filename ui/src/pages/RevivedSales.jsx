3e66f75d1114cd3e12411b02e93dc463b3874411
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-31 16:05:51 -0500

    Create RevivedSales.jsx

---
+++ ui/src/pages/RevivedSales.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RevivedAssetsTable from '../modules/assets/RevivedAssetsTable.jsx';
+
+export default function RevivedSales() {
+  return <RevivedAssetsTable />;
+}
