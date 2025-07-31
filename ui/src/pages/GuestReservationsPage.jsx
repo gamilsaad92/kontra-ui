5bd4424b1467ed1e9ea0af402aab9ac059f2a833
gamilsaad92 <gamilsaad92@gmail.com>
2025-07-31 16:02:38 -0500

    Create GuestReservationsPage.jsx

---
+++ ui/src/pages/GuestReservationsPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import GuestReservations from '../components/GuestReservations.jsx';
+
+export default function GuestReservationsPage() {
+  return <GuestReservations />;
+}
