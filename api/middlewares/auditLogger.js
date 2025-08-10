168a12377301f1542528d7ccc3adbd0c38f99918
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-10 08:34:44 +0000

    Update auditLogger.js

--- api/middlewares/auditLogger.js
+++ api/middlewares/auditLogger.js
@@ -2,15 +2,14 @@ const { logAuditEntry } = require('../auditLogger');
 
 module.exports = function auditLogger(req, res, next) {
   res.on('finish', () => {
-    if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method) && res.statusCode < 400) {
-      const entry = {
-        method: req.method,
-        url: req.originalUrl,
-        userId: req.user ? req.user.id : null,
-        data: req.body || {}
-      };
-      logAuditEntry(entry);
-    }
+   const entry = {
+      method: req.method,
+      url: req.originalUrl,
+      status: res.statusCode,
+      userId: req.user ? req.user.id : null,
+      data: req.body || {}
+    };
+    logAuditEntry(entry);
   });
   next();
 };
