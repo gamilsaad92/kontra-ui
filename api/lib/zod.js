d1c80a365d130f8c415c3144cf1698d9554f8ed6
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-12 20:29:30 -0700

    Update zod.js

--- api/lib/zod.js
+++ api/lib/zod.js
@@ -1,25 +1,2 @@
-function chain() {
-  return {
-    parse: data => data,
-    optional() { return this; },
-    min() { return this; },
-    positive() { return this; },
-    int() { return this; },
-    default() { return this; }
-  };
-}
-
-const z = {
-  object: () => ({
-    parse: data => data,
-    partial() { return this; }
-  }),
-  string: chain,
-  number: chain,
-  enum: () => chain(),
-  array: () => chain(),
-  record: () => ({ parse: data => data }),
-  any: chain
-};
-
-module.exports = { z };
+module.exports = { z, ZodError } = require('zod');
+ 
