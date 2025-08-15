079168370f019fb69a77da31c7bcffce4f3dc884
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-14 22:28:32 -0700

    Update card.tsx

--- ui/src/components/ui/card.tsx
+++ ui/src/components/ui/card.tsx
@@ -1,21 +1,41 @@
 import * as React from "react";
 
-export function Card(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} />;
+function cn(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export function CardHeader(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} />;
-}
+export const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("rounded-2xl border bg-white text-gray-900 shadow-sm", className)} {...props} />
+  )
+);
 
-export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} />;
-}
+export const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-4 border-b", className)} {...props} />
+  )
+);
 
-export function CardFooter(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} />;
-}
+export const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => (
+    <h3 ref={ref} className={cn("text-lg font-semibold", className)} {...props} />
+  )
+);
 
-export function CardTitle(props: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <h3 {...props} />;
-}
+export const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-4", className)} {...props} />
+  )
+);
+
+export const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-4 border-t", className)} {...props} />
+  )
+);
+
+Card.displayName = "Card";
+CardHeader.displayName = "CardHeader";
+CardTitle.displayName = "CardTitle";
+CardContent.displayName = "CardContent";
+CardFooter.displayName = "CardFooter";
