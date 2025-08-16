025f2664f907e191e5ca042a12e8d917da7c396b
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-16 10:09:45 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,28 +1,8 @@
 // ui/src/App.jsx
 
-import React, { useContext, useState } from 'react'
-import KontraDashboard from './components/KontraDashboard'
-import LoginForm from './components/LoginForm'
-import SignUpForm from './components/SignUpForm'
-import InviteAcceptForm from './components/InviteAcceptForm'
-import { AuthContext } from './lib/authContext'
+import React from 'react'
+import DashboardLayout from './components/DashboardLayout'
 
 export default function App() {
-  const { session } = useContext(AuthContext)
-  const [signUp, setSignUp] = useState(false)
-  const params = new URLSearchParams(window.location.search)
-  const inviteToken = params.get('invite')
-  
-  if (!session) {
-    if (inviteToken) {
-      return <InviteAcceptForm token={inviteToken} />
-    }
-    return signUp ? (
-      <SignUpForm onSwitch={() => setSignUp(false)} />
-    ) : (
-      <LoginForm onSwitch={() => setSignUp(true)} />
-    )
-  }
-
- return <KontraDashboard role="lender" orgName="Kontra" userName="Jamil" />
+   return <DashboardLayout role="lender" orgName="Kontra" userName="Jamil" />
 }
