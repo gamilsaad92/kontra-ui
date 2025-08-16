2cffb061d0064bb3ae1634f953393fe22779e205
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-16 09:06:17 -0700

    Update RootLayout.jsx

--- ui/src/RootLayout.jsx
+++ ui/src/RootLayout.jsx
@@ -9,21 +9,23 @@ import {
 
 export default function RootLayout({ children }) {
   return (
-    <div className="bg-slate-950 text-slate-100 min-h-screen">
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton>
-            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-      {children}
-    </div>
+    <html lang="en" className="dark">
+      <body className="bg-slate-950 text-slate-100">
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        {children}
+      </body>
+    </html>
   )
 }
