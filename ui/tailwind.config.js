c0bc480c28dfb6476b5330ec321e029ca9e526cf
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-16 09:12:19 -0700

    Update tailwind.config.js

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,21 +1,6 @@
-const theme = require('./src/theme.js');
-
 module.exports = {
   darkMode: 'class',
-  content: [
-    './index.html',
-    './app/**/*.{ts,tsx}',
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: theme.colors,
-      spacing: theme.spacing,
-      fontFamily: theme.fontFamily,
-      fontSize: theme.fontSize,
-    },
-  },
-  plugins: [],
+ content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: { extend: {} },
+  plugins: []
 }
