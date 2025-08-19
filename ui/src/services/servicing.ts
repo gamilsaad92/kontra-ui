3fa5342b38f32461fb98bb677b4b325190a13adf
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-19 11:04:49 -0700

    Create servicing.ts

---
+++ ui/src/services/servicing.ts
@@ -0,0 +1,4 @@
+import { api, withOrg } from "../lib/api";
+export const listPipeline = async () => (await api.get("/applications?limit=6", withOrg(1))).data;
+export const listDrawRequests = async () => (await api.get("/draws?limit=6", withOrg(1))).data;
+export const sendPayoffQuote = async (loanId:number) => (await api.post(`/loans/${loanId}/payoff-quote`, {}, withOrg(1))).data;
