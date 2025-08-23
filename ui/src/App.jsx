7e8951db28e60fa11c2e19206251a51fb94a114d
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-23 10:47:40 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,13 +1,8 @@
 // ui/src/App.jsx
 
 import React from 'react'
-import DashboardLayout from './components/DashboardLayout'
-import KontraDashboard from '@/components/KontraDashboard'
+import ExecutiveDashboard from './components/ExecutiveDashboard'
 
 export default function App() {
-  return import.meta.env.NEXT_PUBLIC_NEW_DASH ? (
-<KontraDashboard apiBase={import.meta.env.VITE_API_BASE} initialRole="lender" />
-  ) : (
-    <DashboardLayout role="lender" orgName="Kontra" userName="Jamil" />
-  )
+  return <ExecutiveDashboard />
 }
