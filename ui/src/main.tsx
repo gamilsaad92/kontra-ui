d865724fa4533f21daa705ed52809ef85b59bc52
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-23 15:05:15 -0700

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,14 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // Vite resolves .tsx automatically
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
 
