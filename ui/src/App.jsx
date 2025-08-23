a61d2846f0535b409d9aa918cb1d1dede7490c77
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-23 15:34:44 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import DashboardShell from "./layouts/DashboardShell";
+import DashboardShell from "./components/DashboardShell.jsx";
 import PortfolioOverview from "./pages/Lender/PortfolioOverview";
 
 function Placeholder({ title }) {
