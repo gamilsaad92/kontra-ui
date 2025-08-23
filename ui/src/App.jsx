8ed773e29725036c39b063946bb0b612d9425529
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-23 12:16:00 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,24 +2,15 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import DashboardLayout from "./components/DashboardLayout"
-import Loans from "./pages/Loans"
-import Applications from "./pages/Applications"
-import NotFound from "./pages/NotFound"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* default to /dashboard */}
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<DashboardLayout />} />
-
-        {/* example sections you can wire later */}
-        <Route path="/loans" element={<Loans />} />
-        <Route path="/applications" element={<Applications />} />
-
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
+        {/* remove other routes for now */}
+        <Route path="*" element={<DashboardLayout />} />
       </Routes>
     </BrowserRouter>
   )
