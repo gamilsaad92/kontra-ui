e84d76dfdf429a5f3351764c8a492db5de831800
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-26 01:36:18 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,38 +1,21 @@
 // ui/src/App.jsx
 
-import React, { useContext, useState } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { SignedIn } from '@clerk/clerk-react'
 import DashboardLayout from './components/DashboardLayout'
 import DashboardShell from './components/DashboardShell'
-import LoginForm from './components/LoginForm'
-import SignUpForm from './components/SignUpForm'
-import InviteAcceptForm from './components/InviteAcceptForm'
 import KontraDashboard from './pages/KontraDashboard'
-import { AuthContext } from './lib/authContext'
 
 export default function App() {
-  const { session } = useContext(AuthContext)
-  const [signUp, setSignUp] = useState(false)
-  const params = new URLSearchParams(window.location.search)
-  const inviteToken = params.get('invite')
-  
-  if (!session) {
-    if (inviteToken) {
-      return <InviteAcceptForm token={inviteToken} />
-    }
-    return signUp ? (
-      <SignUpForm onSwitch={() => setSignUp(false)} />
-    ) : (
-      <LoginForm onSwitch={() => setSignUp(true)} />
-    )
-  }
-
     return (
-    <Routes>
-      <Route element={<DashboardShell />}>
-        <Route path="/dashboard/kontra" element={<KontraDashboard />} />
-      </Route>
-      <Route path="/*" element={<DashboardLayout />} />
-    </Routes>
+    <SignedIn>
+      <Routes>
+        <Route element={<DashboardShell />}>
+          <Route path="/dashboard/kontra" element={<KontraDashboard />} />
+        </Route>
+        <Route path="/*" element={<DashboardLayout />} />
+      </Routes>
+    </SignedIn>
   )
 }
