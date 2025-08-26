2732aad80a4c032773a7620a4bc2f4c14036ec50
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-26 01:59:21 -0700

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,20 +2,17 @@
 
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { SignedIn } from '@clerk/clerk-react'
 import DashboardLayout from './components/DashboardLayout'
 import DashboardShell from './components/DashboardShell'
 import KontraDashboard from './pages/KontraDashboard'
 
 export default function App() {
-    return (
-    <SignedIn>
-      <Routes>
-        <Route element={<DashboardShell />}>
-          <Route path="/dashboard/kontra" element={<KontraDashboard />} />
-        </Route>
-        <Route path="/*" element={<DashboardLayout />} />
-      </Routes>
-    </SignedIn>
+   return (
+    <Routes>
+      <Route element={<DashboardShell />}>
+        <Route path="/dashboard/kontra" element={<KontraDashboard />} />
+      </Route>
+      <Route path="/*" element={<DashboardLayout />} />
+    </Routes>
   )
 }
