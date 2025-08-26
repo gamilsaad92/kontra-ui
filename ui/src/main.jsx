9a4e8537a4547aa33759f1fce671a4a5b00aebc4
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-25 20:27:29 -0700

    Update main.jsx

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // resolves ./App.jsx automatically
 import "./index.css";
+import { QueryClientProvider } from "./lib/queryClient";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <App />
+     <QueryClientProvider>
+        <App />
+      </QueryClientProvider>
   </React.StrictMode>,
 );
