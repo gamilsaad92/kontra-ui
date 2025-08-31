a29f941d74b85bbb6fd65029187c70218eb842f8
gamilsaad92 <gamilsaad92@gmail.com>
2025-08-30 23:01:18 -0700

    Update main.jsx

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
-
-const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY!;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkKey} afterSignOutUrl="/" signInUrl="/sign-in" signUpUrl="/sign-up">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+     <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
