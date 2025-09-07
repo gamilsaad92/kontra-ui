96d9d8472d3328c34a95ebc43d205dcc691ecbe3
gamilsaad92 <gamilsaad92@gmail.com>
2025-09-07 14:10:40 -0700

    Create LeftIcon.tsx

---
+++ ui/src/components/LeftIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LeftIcon() {
+  return (
+    <svg viewBox="0 0 24 24" className="h-4 w-4 shrink-0" aria-hidden>
+      <path
+        className="fill-current"
+        d="M12 2a10 10 0 1 0 .001 20.001A10 10 0 0 0 12 2Zm0 4.75a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5Zm-2 4.5h4a1 1 0 1 1 0 2h-1v4a1 1 0 1 1-2 0v-4h-1a1 1 0 1 1 0-2Z"
+      />
+    </svg>
+  );
+}
