29b2318ac55fb6ff7c61574c6b028297cd96d6db
gamilsaad92 <gamilsaad92@gmail.com>
2025-09-08 17:13:50 -0700

    Create DrawsDashboard.jsx

---
+++ ui/src/components/DrawsDashboard.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import DrawRequestForm from './DrawRequestForm';
+import DrawRequestsTable from './DrawRequestsTable';
+import LienWaiverForm from './LienWaiverForm';
+import LienWaiverList from './LienWaiverList';
+
+export default function DrawsDashboard() {
+  const [selected, setSelected] = useState(null);
+  const [refresh, setRefresh] = useState(0);
+
+  return (
+    <div className="space-y-6">
+      <DrawRequestForm onSubmitted={id => setSelected(id)} />
+      {selected && (
+        <>
+          <LienWaiverForm drawId={selected} onUploaded={() => setRefresh(r => r + 1)} />
+          <LienWaiverList filter={{ draw_id: selected, refresh }} />
+        </>
+      )}
+      <DrawRequestsTable onSelect={id => setSelected(id)} canReview />
+    </div>
+  );
+}
