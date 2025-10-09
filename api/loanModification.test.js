5291f573e45676270cb21980ac6875e5bc4aad9f
gamilsaad92 <gamilsaad92@gmail.com>
2025-10-08 20:44:53 -0700

    Update loanModification.test.js

--- api/loanModification.test.js
+++ api/loanModification.test.js
@@ -1,9 +1,16 @@
 const request = require('supertest');
-const app = require('../index');
+const { app, resetSupabase } = require('./testUtils/app');
+
+beforeEach(() => {
+  resetSupabase();
+});
 
 describe('POST /api/loans/:id/defer', () => {
   it('requires months', async () => {
-    const res = await request(app).post('/api/loans/1/defer').send({});
+    const res = await request(app)
+      .post('/api/loans/1/defer')
+      .set('Authorization', 'Bearer test-token')
+      .send({});
     expect(res.statusCode).toBe(400);
   });
 });
