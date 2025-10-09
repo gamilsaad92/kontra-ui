38d5b654e556b33622ef64affe35ee1df89fdb6c
gamilsaad92 <gamilsaad92@gmail.com>
2025-10-08 21:06:08 -0700

    Update selfService.test.js

--- api/selfService.test.js
+++ api/selfService.test.js
@@ -1,16 +1,23 @@
 const request = require('supertest');
-const app = require('../index');
+const { app, resetSupabase } = require('./testUtils/app');
+
+beforeEach(() => {
+  resetSupabase();
+});
 
 describe('GET /api/loans/:id/balance', () => {
   it('endpoint exists', async () => {
     const res = await request(app).get('/api/loans/1/balance');
-    expect(res.statusCode).not.toBe(404);
+   expect(res.statusCode).toBe(401);
   });
 });
 
 describe('POST /api/loans/:id/payoff', () => {
   it('requires payoff_date', async () => {
-    const res = await request(app).post('/api/loans/1/payoff').send({});
+    const res = await request(app)
+      .post('/api/loans/1/payoff')
+      .set('Authorization', 'Bearer test-token')
+      .send({});
     expect(res.statusCode).toBe(400);
   });
 });
