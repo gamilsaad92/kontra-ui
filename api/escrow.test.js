659bd3162ad05181f2228732016ae9583fb1d27b
gamilsaad92 <gamilsaad92@gmail.com>
2025-10-08 20:44:03 -0700

    Update escrow.test.js

--- api/escrow.test.js
+++ api/escrow.test.js
@@ -1,9 +1,15 @@
 const request = require('supertest');
-const app = require('../index');
+const { app, resetSupabase } = require('./testUtils/app');
+
+beforeEach(() => {
+  resetSupabase();
+});
 
 describe('GET /api/loans/:id/escrow', () => {
   it('validates loan id', async () => {
-    const res = await request(app).get('/api/loans/foo/escrow');
+    const res = await request(app)
+      .get('/api/loans/foo/escrow')
+      .set('Authorization', 'Bearer test-token');
     expect(res.statusCode).toBe(400);
   });
 });
