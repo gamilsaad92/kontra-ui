7ca635b7a55d7e3e15a69faad09430f69f390b54
gamilsaad92 <gamilsaad92@gmail.com>
2025-10-17 19:15:02 -0700

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -5,8 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import { installAuthFetchInterceptor } from "./lib/http";
 
 const queryClient = new QueryClient();
+
+installAuthFetchInterceptor();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
