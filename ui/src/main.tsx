0e46cde883d9cb20cc2615f3bbd23d9253654475
gamilsaad92 <gamilsaad92@gmail.com>
2025-10-29 10:59:47 -0700

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 import { installAuthFetchInterceptor } from "./lib/http";
+import { AuthProvider } from "./lib/authContext";
 
 const queryClient = new QueryClient();
 
@@ -13,10 +14,12 @@ installAuthFetchInterceptor();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-       <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+   <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
