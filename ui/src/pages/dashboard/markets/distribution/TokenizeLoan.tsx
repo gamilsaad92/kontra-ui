198d63628848cb24bcac430420c90e4b2213ab7e
gamilsaad92 <gamilsaad92@gmail.com>
2026-01-30 16:37:11 -0800

    Update TokenizeLoan.tsx

--- ui/src/pages/dashboard/markets/distribution/TokenizeLoan.tsx
+++ ui/src/pages/dashboard/markets/distribution/TokenizeLoan.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { api } from "../../../lib/api";
+import { api } from "../../../../lib/api";
 
 const defaultMetadata = {
   collateral_summary: { type: "Multifamily", occupancy: "94%" },
