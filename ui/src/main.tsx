e7bbe2f8644bb9adcf2461ff6b5fac3c91857359
gamilsaad92 <gamilsaad92@gmail.com>
2026-01-31 17:18:48 -0800

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -5,14 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
-import { installAuthFetchInterceptor } from "./lib/http";
+import { installApiFetchInterceptor } from "./lib/apiClient";
 import { AuthProvider } from "./lib/authContext";
 import { Web3Provider } from "./providers/Web3Provider";
 import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
 
-installAuthFetchInterceptor();
+installApiFetchInterceptor();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
