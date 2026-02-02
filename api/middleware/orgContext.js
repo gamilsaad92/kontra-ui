5076af8c045e34ec69b4acf3d6c20bb5a102c554
gamilsaad92 <gamilsaad92@gmail.com>
2026-02-01 23:03:06 -0800

    Create orgContext.js

---
+++ api/middleware/orgContext.js
@@ -0,0 +1,21 @@
+const orgContext = (req, res, next) => {
+  const headerOrg =
+    req.headers['x-organization-id'] ||
+    req.headers['x-org-id'] ||
+    req.headers['organization-id'];
+
+  const orgId = headerOrg || req.query.orgId || req.body?.orgId || req.body?.organizationId;
+
+  const userId = req.headers['x-user-id'] || req.query.userId || req.body?.userId;
+
+  if (!orgId) {
+    return res.status(400).json({ message: 'Missing organization id' });
+  }
+
+  req.orgId = String(orgId);
+  req.userId = userId ? String(userId) : null;
+
+  next();
+};
+
+module.exports = { orgContext };
