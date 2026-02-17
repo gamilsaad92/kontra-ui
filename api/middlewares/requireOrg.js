7594d206ee25f1b3b16a975f57e7db7d5b2e97d1
gamilsaad92 <gamilsaad92@gmail.com>
2026-02-16 23:10:16 -0800

    Update requireOrg.js

--- api/middlewares/requireOrg.js
+++ api/middlewares/requireOrg.js
@@ -2,7 +2,7 @@ module.exports = function requireOrg(req, res, next) {
    const orgId =
     req.organizationId || req.headers['x-organization-id'] || req.headers['x-org-id'];
   if (!orgId) {
-    return res.status(400).json({ message: 'Missing organization id' });
+     return res.status(400).json({ code: 'ORG_CONTEXT_MISSING', message: 'Missing X-Org-Id header' });
   }
   req.orgId = orgId;
   next();
