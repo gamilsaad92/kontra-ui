95cbf565a544a4234262fc8adf0828a68fc0670f
gamilsaad92 <gamilsaad92@gmail.com>
2026-03-08 23:20:00 -0700

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -79,19 +78,17 @@ if (typeof window !== "undefined") {
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <LocaleProvider>
-        <AuthProvider>
-          <QueryClientProvider client={queryClient}>
-            <Web3Provider>
-              <BrowserRouter>
-                <App />
-              </BrowserRouter>
-            </Web3Provider>
-          </QueryClientProvider>
-        </AuthProvider>
-      </LocaleProvider>
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <LocaleProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <Web3Provider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Web3Provider>
+        </QueryClientProvider>
+      </AuthProvider>
+    </LocaleProvider>
+  </ErrorBoundary>
 );
