fe10211c03c0714325be567e1a7c66813ca4cef7
gamilsaad92 <gamilsaad92@gmail.com>
2026-03-22 19:37:17 -0700

    Update orgContext.js

--- api/middleware/orgContext.js
+++ api/middleware/orgContext.js
@@ -1,21 +1,5 @@
-const orgContext = (req, res, next) => {
-  const headerOrg =
-    req.headers['x-organization-id'] ||
-    req.headers['x-org-id'] ||
-    req.headers['organization-id'];
+const requireOrg = require('../middlewares/requireOrg');
 
-  const orgId = headerOrg || req.query.orgId || req.body?.orgId || req.body?.organizationId;
-
-  const userId = req.headers['x-user-id'] || req.query.userId || req.body?.userId;
-
-  if (!orgId) {
-    return res.status(400).json({ code: 'ORG_CONTEXT_MISSING', message: 'Missing X-Org-Id header' });
-  }
-
-  req.orgId = String(orgId);
-  req.userId = userId ? String(userId) : null;
-
-  next();
-};
+const orgContext = (req, res, next) => requireOrg(req, res, next);
 
 module.exports = { orgContext };
